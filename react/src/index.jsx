fe4175459b93cc7dcd6a8bb209f69d72f068bb66
John <johntduong@users.noreply.github.com>
2017-03-15 16:12:11 -0700

    Update index.jsx

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,20 +7,6 @@ class App extends React.Component {
     this.state = { 
       items: []
     }
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
   }
 
   render () {
