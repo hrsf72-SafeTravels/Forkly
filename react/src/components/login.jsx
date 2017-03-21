b8778945594e4ed0f6d44c1993c878736e44bb91
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-03-21 02:20:54 -0700

    (feat) Comment out properties on post req

--- react/src/components/login.jsx
+++ react/src/components/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
+// import $ from 'jquery';
 
 class Login extends React.Component {
   constructor(props) {
@@ -34,7 +34,7 @@ class Login extends React.Component {
     $.ajax({
       url: '/login',
       type: 'POST',
-      contentType: 'application/JSON',
+      // contentType: 'application/JSON',
       data: JSON.stringify(user),
       success: function(data) {
         console.log('successful login post');
