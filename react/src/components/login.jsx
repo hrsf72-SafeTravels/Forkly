9676ef025a284c8c8e73dec8ebd7f5e200f19fd0
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-03-21 02:10:47 -0700

    (feat) Add login submit handler

--- react/src/components/login.jsx
+++ react/src/components/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,11 +10,38 @@ class Login extends React.Component {
       password: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
     this.setState({
       [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // server needs to check if username exists in db and if password is valid
+    // send username and password to server
+    event.preventDefault();
+
+    console.log('username: ', this.state.username);
+
+    var user = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    $.ajax({
+      url: '/login',
+      type: 'POST',
+      contentType: 'application/JSON',
+      data: JSON.stringify(user),
+      success: function(data) {
+        console.log('successful login post');
+      },
+      error: function(err) {
+        console.log('login error');
+      }
     });
   }
 
@@ -24,15 +52,16 @@ class Login extends React.Component {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Username: 
             <input name="username" type="text" value={this.state.username} onChange={this.handleInputChange}/>
           </label>
           <label>
             Password: 
-            <input name="password" type="text" value={this.state.password} onChange={this.handleInputChange}/>
+            <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
+          <input type="submit"/>
         </form>
       </div>
     )
