85130c6715cdc0533d3a182405510152fa6e0c7a
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-23 12:18:59 -0700

    add /api/addRecipe route to server to listed for post requests from addrecipe

--- server/index.js
+++ server/index.js
@@ -4,9 +4,16 @@ var items = require('../db');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static(__dirname + '/../react/dist'));
+
+app.post('/api/addRecipe', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
-
