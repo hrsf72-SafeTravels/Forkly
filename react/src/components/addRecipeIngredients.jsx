a0ab8723795868400f4377ffd688e3176e9a1b5b
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-23 12:16:49 -0700

    implement state in addRecipe to track the form data
    This commit moves the state out of addrecipeingredients and into
    addrecipe. This is so that when we submit the form an ajax request can
    read the state of addrecipe and send the state to the server. The
    handleSubmit function is not fully implemented yet, but it will be used
    to actually send the data.

--- react/src/components/addRecipeIngredients.jsx
+++ react/src/components/addRecipeIngredients.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 
 class AddRecipeIngredients extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      addRecipeQuantity: 0,
-      addRecipeUnits: 'units',
-      addRecipeIngredient: 'ingredients'
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleInputChange (event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-    this.setState({
-      [name]: value
-    });
+
+  handleChange(event) {
+    this.props.handleIngredientsChange(event, this.props.index)
   }
 
   render () {
-    return (
-      <tbody>
-        <tr>
-          <td><input type="number" name="addRecipeQuantity" value={this.state.quantity} onChange={this.handleInputChange} /></td>
-          <td><input type="text" name="addRecipeUnits" value={this.state.units} onChange={this.handleInputChange} /></td>
-          <td><input type="text" name="addRecipeIngredient" value={this.state.ingredients} onChange={this.handleInputChange}/></td>
-          <td><input type="button" name="addRecipeNewRow" value="Add Row" onClick={this.props.addRow}/></td>
-        </tr>
-      </tbody>
-    )
+    if (this.props.showButton) {
+      return (
+        <tbody>
+          <tr>
+            <td><input type="number" name="addRecipeQuantity" value={this.props.addRecipeQuantity} onChange={this.handleChange} /></td>
+            <td><input type="text" name="addRecipeUnits" value={this.props.addRecipeUnits} onChange={this.handleChange} /></td>
+            <td><input type="text" name="addRecipeIngredient" value={this.props.addRecipeIngredient} onChange={this.handleChange}/></td>
+            <td><input type="button" name="addRecipeNewRow" value="Add Row" onClick={this.props.addRow}/></td>
+          </tr>
+        </tbody>
+      )
+    } else {
+      return (
+        <tbody>
+          <tr>
+            <td><input type="number" name="addRecipeQuantity" value={this.props.addRecipeQuantity} onChange={this.handleChange} /></td>
+            <td><input type="text" name="addRecipeUnits" value={this.props.addRecipeUnits} onChange={this.handleChange} /></td>
+            <td><input type="text" name="addRecipeIngredient" value={this.props.addRecipeIngredient} onChange={this.handleChange}/></td>
+          </tr>
+        </tbody>
+      )
+    }
   }
 }
 
