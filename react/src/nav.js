0b50786b075b3415c5baf07cebc3a1ec3c2cafaf
Tina Ko <tinarchan@gmail.com>
2017-03-22 17:10:37 -0700

    Style Navigation page with image/icons that change to text upon hover
    Still need to work on floating left and right correctly, as the title and navigation icons are not on the same line

--- react/src/nav.js
+++ react/src/nav.js
@@ -6,18 +6,39 @@ class Nav extends React.Component {
   }
 
   render () {
-  	console.log(this.props);
+  	// console.log(this.props);
   	return (
-  	  <div> 
-		<h1>FORKLY</h1>
-		<h3>Welcome, {this.props.username}</h3>
-		<button>Home</button>
-		<button>My Forks</button>
-		<button>Add Recipe</button>
-	  </div>
+  	  <div className="nav"> 
+        <section className="group">
+      		<h1 className="title logo">FORKLY</h1>
+      		<h3 className="title username">Welcome, {this.props.username}</h3>
+          <nav>
+            <div className="icon">
+              <img className="navButton" src="assets/images/logout.png" alt="Logout"/>
+              <span>Logout</span>
+            </div>
+            <div className="icon">
+              <img className="navButton" src="assets/images/addRecipe.png" alt="Add Recipe"/>
+              <span>Add Recipe</span>
+            </div>
+            <div className="icon">
+              <img className="navButton" src="assets/images/fork.png" alt="My Forks"/>
+              <span>My Forks</span>
+            </div>
+            <div className="icon">
+              <img className="navButton" src="assets/images/home.png" alt="Home"/>
+              <span>Home</span>
+            </div>
+          </nav>
+        </section>
+	   </div>
 	  )
   }
 
 }
 
 export default Nav;
+
+// <a className="navImage home" src="assets/images/home.jpg">Home</a>
+// <a className="navImage myForks">My Forks</a>
+// <a className="navImage addRecipe">Add Recipe</a>
