9547cf654145a1006380503325011797f3c2ec3b
Tina Ko <tinarchan@gmail.com>
2017-03-24 16:52:54 -0700

    Add minor updates to viewFork

--- react/src/viewFork.js
+++ react/src/viewFork.js
@@ -33,6 +33,17 @@ class ViewFork extends React.Component {
   render() {
   	return (
   	  <div>
+        <div className='recipeName'>
+          <h3><em>{this.props.recipe.name}</em></h3>
+        </div>
+        <div className='ingredients'>
+          <h4>Ingredients</h4>
+          <p>{this.props.recipe.ingredients}</p>
+        </div>
+        <div className='directions'>
+          <h4>Directions</h4>
+          <p>{this.props.recipe.directions}</p>
+        </div>
   	  </div>
   	)
   }
