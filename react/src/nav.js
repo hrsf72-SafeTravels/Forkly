d15d4eab59ae4f300d21560cbde35e91e2f3f26f
Tina Ko <tinarchan@gmail.com>
2017-03-23 20:19:22 -0700

    Add style class names

--- react/src/nav.js
+++ react/src/nav.js
@@ -37,19 +37,19 @@ class Nav extends React.Component {
       		<h1 className="title logo">FORKLY</h1>
       		<h3 className="title username">Welcome, {this.state.username}</h3>
           <nav>
-            <div className="icon">
+            <div className="icon logout">
               <img className="navButton" src="assets/images/logout.png" alt="Logout"/>
               <span>Logout</span>
             </div>
-            <div className="icon">
+            <div className="icon addRecipe">
               <img className="navButton" src="assets/images/addRecipe.png" alt="Add Recipe"/>
               <span>Add Recipe</span>
             </div>
-            <div className="icon">
+            <div className="icon myForks">
               <img className="navButton" src="assets/images/fork.png" alt="My Forks"/>
               <span>My Forks</span>
             </div>
-            <div className="icon">
+            <div className="icon home">
               <img className="navButton" src="assets/images/home.png" alt="Home"/>
               <span>Home</span>
             </div>
