75275298d8253ee54d3e267e0c559d6f7db7cad6
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-25 14:28:52 -0700

    add setup.example.js file to project
    this file lets you set a client_id and client_secret in your env so the
    id and secret are kept out of git but still compatible with heroku

---
+++ server/setup.example.js
@@ -0,0 +1,3 @@
+process.env.CLIENT_ID = process.env.CLIENT_ID || 'CLIENT_ID';
+process.env.CLIENT_SECRET = process.env.CLIENT_SECRET || 'CLIENT_SECRET';
+process.env.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/forkly-dev';
