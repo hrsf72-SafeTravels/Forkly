8c726a7bec41403de3734d0e5cac1199cf4b3c1e
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-03-25 18:28:59 -0700

    (feat) Adjust facebook authentication callback url for deployment

--- react/env/config.js
+++ react/env/config.js
@@ -3,6 +3,6 @@ module.exports = {
   {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/facebook/callback"
+      callbackURL: process.env.SITE_URL + 'auth/facebook/callback'
     }
 }
--- server/setup.example.js
+++ server/setup.example.js
@@ -1,3 +1,4 @@
 process.env.CLIENT_ID = 'CLIENT_ID';
 process.env.CLIENT_SECRET = 'CLIENT_SECRET';
 process.env.MONGODB_URI = 'mongodb://localhost/forkly-dev';
+process.env.SITE_URL = 'http://localhost:3000/';
