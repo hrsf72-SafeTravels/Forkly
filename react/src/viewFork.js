81bc19a6b9ed81526a90a32b954d8e73b5b41f4b
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-26 18:12:57 -0700

    change viewFork to be more similar to addRecipe
    this is to set up the view of things to be similar to addRecipe but
    without the form, making things static instead

--- react/src/viewFork.js
+++ react/src/viewFork.js
@@ -3,51 +3,107 @@ import $ from 'jquery';
 
 class ViewFork extends React.Component {
   constructor(props) {
-  	super(props);
-    this.state = {};
+    super(props);
+    this.state = {
+      name: 'Rum Ham',
+      directions: 'Soak in rum and bake',
+      ingredients: [{quantity: 1, units: 'whole', ingredient: 'ham'}, {quantity: 1, units: 'gallon', ingredient: 'rum'}]
+    }
+    // this.addRow = this.addRow.bind(this);
+    // this.handleIngredientsChange = this.handleIngredientsChange.bind(this);
+    // this.handleInputChange = this.handleInputChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
+  // navigate() {
+  //   // const { router } = this.context
+  //   var router = this.context.router;
+  //   router.history.push('/')
+  // }
 
-  searchRecipe(searchTerm) {
-    var context = this;
+  // handleSubmit (event) {
+  //   event.preventDefault();
+  //   $.ajax({
+  //     url: "/api/viewFork",
+  //     data: JSON.stringify(this.state),
+  //     method: 'POST',
+  //     contentType: 'application/JSON',
+  //     error: (error) => {
+  //       console.log('you have an error', error);
+  //     },
+  //     success: (result) => {
+  //       //implement a redirect to the users recipe page
+  //       console.log('POST successful');
+  //       this.navigate();
+  //     }
+  //   });
+  // }
 
-    $.ajax({
-      url: '/searchRecipes',
-      type:'POST',
-      data: JSON.stringify(searchTerm),
-      // type: 'GET',
-      // data: searchTerm,
-      contentType: 'application/json',
-      // upon success, adds results to this.state.recipes
-      success: function(data){
-        console.log('ajax request to search recipes was successful!');
-        console.log('response', data);
-        context.setState({recipes: data});
-        
-      },
-      error: function(err) {
-        console.log('ajax request to search recipes failed');
-      }
-    });
-  };
+  // addRow() {
+  //   let myIngredients = this.state.ingredients;
+  //   myIngredients[myIngredients.length - 1].showButton = false;
+  //   myIngredients.push({quantity: 0, units: '', ingredient: '', showButton: true});
+  //   this.setState({ingredients: myIngredients});
+  // }
 
-  render() {
-  	return (
-  	  <div>
-        <div className='recipeName'>
-          <h3><em>{this.props.recipe.name}</em></h3>
-        </div>
-        <div className='ingredients'>
-          <h4>Ingredients</h4>
-          <p>{this.props.recipe.ingredients}</p>
-        </div>
-        <div className='directions'>
-          <h4>Directions</h4>
-          <p>{this.props.recipe.directions}</p>
-        </div>
-  	  </div>
-  	)
+  // handleIngredientsChange (event, index) {
+  //   const target = event.target;
+  //   const name = target.name;
+  //   const value = target.value;
+
+  //   let ing = this.state.ingredients;
+  //   ing[index][name] = value;
+
+  //   this.setState({
+  //     ingredients: ing
+  //   });
+  // }
+
+  // handleInputChange (event) {
+  //   const target = event.target;
+  //   const name = target.name;
+  //   const value = target.value;
+
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // }
+
+  render () {
+    return (
+      <div>
+        <header>
+          <h1>{this.state.name}</h1>
+        </header>
+        <table>
+          <thead>
+            <tr>
+              <td>Quantity</td>
+              <td>Units</td>
+              <td>Ingredient</td>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.ingredients.map(function(val, index) {
+            return (
+              <tr>
+                <td>{val.quantity}</td>
+                <td>{val.units}</td>
+                <td>{val.ingredient}</td>
+              </tr>
+            )
+           }, this)}
+          </tbody>
+        </table>
+        Directions: <br />
+        <p>{this.state.directions}</p>
+      </div>
+    )
   }
 }
 
-export default ViewFork; 
+// viewFork.contextTypes = {
+//   router: React.PropTypes.object
+// }
 
+export default ViewFork;
