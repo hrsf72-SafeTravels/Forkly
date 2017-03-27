613eb1cdf8ec4eec01b4425494bbd8a2f46e97d8
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-26 20:09:11 -0700

    change viewFork to stateless component
    A recipe will be passed down through props to viewFork to be rendered.
    This way we don't have to have any state stored in viewFork.

--- react/src/viewFork.js
+++ react/src/viewFork.js
@@ -1,64 +1,36 @@
 import React from 'react';
-import $ from 'jquery';
 
-class ViewFork extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Rum Ham',
-      directions: 'Soak in rum and bake',
-      ingredients: [{quantity: 1, units: 'whole', ingredient: 'ham'}, {quantity: 1, units: 'gallon', ingredient: 'rum'}]
-    }
-  }
-
-  componentWillMount () {
-    $.ajax({
-      url: "/searchRecipes",
-      data: JSON.stringify(this.state),
-      method: 'GET',
-      contentType: 'application/JSON',
-      error: (error) => {
-        console.log('Error trying to search for recipes', error);
-      },
-      success: (result) => {
-        //implement a redirect to the users recipe page
-        console.log('POST successful');
-        this.navigate();
-      }
-    });
-  }
-
-  render () {
-    return (
-      <div>
-        <header>
-          <h1>{this.state.name}</h1>
-        </header>
-        <table>
-          <thead>
+const ViewFork = () => {
+  let { recipe } = this.props;
+  return (
+    <div>
+      <header>
+        <h1>{recipe.name}</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <td>Quantity</td>
+            <td>Units</td>
+            <td>Ingredient</td>
+          </tr>
+        </thead>
+        <tbody>
+        {recipe.ingredients.map(function(val, index) {
+          return (
             <tr>
-              <td>Quantity</td>
-              <td>Units</td>
-              <td>Ingredient</td>
+              <td>{val.quantity}</td>
+              <td>{val.units}</td>
+              <td>{val.ingredient}</td>
             </tr>
-          </thead>
-          <tbody>
-          {this.state.ingredients.map(function(val, index) {
-            return (
-              <tr>
-                <td>{val.quantity}</td>
-                <td>{val.units}</td>
-                <td>{val.ingredient}</td>
-              </tr>
-            )
-           }, this)}
-          </tbody>
-        </table>
-        Directions: <br />
-        <p>{this.state.directions}</p>
-      </div>
-    )
-  }
+          )
+         }, this)}
+        </tbody>
+      </table>
+      Directions: <br />
+      <p>{recipe.directions}</p>
+    </div>
+  )
 }
 
 export default ViewFork;
