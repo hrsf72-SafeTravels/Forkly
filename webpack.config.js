6aeeb1d2028c2fac3792bcb4d4c305f66185cf83
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-28 19:13:45 -0700

    (feat) resolve extensions such as .jsx and .js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.jsx'],
+  },
   module : {
     loaders : [
       {
