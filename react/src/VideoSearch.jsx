84df0f3b6119d6864421c47f821fcfdc11678675
Jason An <an.jason.1994@gmail.com>
2017-03-29 18:18:49 -0700

    (feat) Include title for videos

--- react/src/VideoSearch.jsx
+++ react/src/VideoSearch.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const VideoSearch = (props) => {
   return (
     <div className="video">
-      <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen />
       <h3>{props.video.snippet.title}</h3>
       <span>{props.video.snippet.description}</span>
+      <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen />
     </div>
   );
 };
