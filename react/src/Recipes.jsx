9d7b3246ecdac7191b4f8d8bbf91b6a684640f1e
Jason An <an.jason.1994@gmail.com>
2017-03-31 12:31:25 -0700

    (refactor) Increase size of embedded videos

--- react/src/Recipes.jsx
+++ react/src/Recipes.jsx
@@ -16,17 +16,17 @@ const Recipes = (props) => {
           slideshowDelay={4000}
         />
       }*/}
-      <div className="results row">
-        <ul>
-          {props.recipes.map((element, index) => <RecipeSearch recipe={element.recipe} id={index} key={index}/>)}
-        </ul>
-      </div>
       <div className="row" className="video-list-container">
           {props.videos.length > 0 &&
             <h2>Recommended Videos</h2>
           }
         <ul className="video-list">
           {props.videos.map((element, index) => <VideoSearch video={element} id={index} key={index} />)}
+        </ul>
+      </div>
+      <div className="results row">
+        <ul>
+          {props.recipes.map((element, index) => <RecipeSearch recipe={element.recipe} id={index} key={index}/>)}
         </ul>
       </div>
     </div>
--- react/src/VideoSearch.jsx
+++ react/src/VideoSearch.jsx
@@ -4,7 +4,7 @@ import VideoDescription from './videoDescription'
 const VideoSearch = (props) => {
   return (
       <li>
-          <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen />
+          <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} height="250px" width="400px"  allowFullScreen />
       </li>
   );
 };
