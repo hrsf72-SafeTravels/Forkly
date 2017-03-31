ce7a65ca4201fa5efccdf04845f699b053ebebc8
Jason An <an.jason.1994@gmail.com>
2017-03-30 19:49:21 -0700

    (feat) Implement simple carousel of youtube videos

--- react/src/Recipes.jsx
+++ react/src/Recipes.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import RecipeSearch from './recipeSearch.jsx';
 import VideoSearch from './VideoSearch.jsx';
+import VideoCarousel from './VideoCarousel.jsx';
 
 const Recipes = (props) => {
   console.log('looking at the props', props)
   return (
     <div className="container">
+      {props.videos.length > 0 && 
+        <VideoCarousel 
+          videos={props.videos}
+          defaultSelectedIndex={0}
+          showThumbnails={true}
+          slideshowActive={true}
+          slideshowDelay={4000}
+        />
+      }
       <div className="results row">
         <ul>
           {props.recipes.map((element, index) => <RecipeSearch recipe={element.recipe} id={index} key={index}/>)}
