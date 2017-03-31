7a57ec24f63c9e0a857364ad8b776c2790f19fbd
Jason An <an.jason.1994@gmail.com>
2017-03-31 12:44:40 -0700

    (feat) Show video description on hover

--- react/src/VideoSearch.jsx
+++ react/src/VideoSearch.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import VideoDescription from './videoDescription'
 
-const VideoSearch = (props) => {
-  return (
-      <li>
-          <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} height="250px" width="400px"  allowFullScreen />
+class VideoSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDescription: false,
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleExit = this.handleExit.bind(this);
+  }
+
+  handleEnter() {
+    this.setState({
+      showDescription: true,
+    });
+  }
+
+  handleExit() {
+    this.setState({
+      showDescription: false,
+    })
+  }
+
+  render() {
+    return (
+      <li onMouseEnter={this.handleEnter} onMouseLeave={this.handleExit}>
+          <iframe src={`https://www.youtube.com/embed/${this.props.video.id.videoId}`} height="250px" width="400px"  allowFullScreen />
+          {this.state.showDescription &&
+            <div className="video-description">
+              <h3>{this.prop.video.snippet.title}</h3>
+              <p>{this.prop.video.snippet.description}</p>
+            </div>
+          }
       </li>
-  );
-};
+    );
+  }
+}
 
 export default VideoSearch;
