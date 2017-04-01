8112a9107abc92db6291ec2b0641e9de41ebd5e7
Jason An <an.jason.1994@gmail.com>
2017-03-31 19:07:07 -0700

    (feat) Create ajax request to fetch top recipes

--- react/src/TopRecipeList.jsx
+++ react/src/TopRecipeList.jsx
@@ -1,107 +1,134 @@
 import React from 'react';
 import TopRecipeEntry from './TopRecipeEntry.jsx';
+import $ from 'jQuery';
 
-const topRatedExampleData = [
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg1',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 100,
-  },
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg2',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 99,
-  },
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg3',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 98,
-  },
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg4',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 97
-  },
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg6',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 95
-  },
-  {
-    name: 'Chicken Cordon Bleu Casserole',
-    ingredients: [
-      '1 egg5',
-      '1/2 cup milk',
-      '2 pounds skinless, boneless chicken breast halves - cut into chunks',
-      '1 cup plain dried bread crumbs',
-      '1 cup oil for frying',
-      '8 ounces Swiss cheese, cubed',
-      '8 ounces cubed ham',
-      '1 (10.75 ounce) can condensed cream of chicken soup',
-      '1 cup milk',
-    ],
-    likes: 96
-  },
-];
+// const topRatedExampleData = [
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg1',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 100,
+//   },
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg2',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 99,
+//   },
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg3',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 98,
+//   },
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg4',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 97
+//   },
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg6',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 95
+//   },
+//   {
+//     name: 'Chicken Cordon Bleu Casserole',
+//     ingredients: [
+//       '1 egg5',
+//       '1/2 cup milk',
+//       '2 pounds skinless, boneless chicken breast halves - cut into chunks',
+//       '1 cup plain dried bread crumbs',
+//       '1 cup oil for frying',
+//       '8 ounces Swiss cheese, cubed',
+//       '8 ounces cubed ham',
+//       '1 (10.75 ounce) can condensed cream of chicken soup',
+//       '1 cup milk',
+//     ],
+//     likes: 96
+//   },
+// ];
 
-const TopRecipeList = () => {
-  return (
-    <div className="container">
-      {topRatedExampleData.sort((a, b) => b.likes - a.likes).map((recipe) =>
-        <TopRecipeEntry recipe={recipe}/>
-      )}
-    </div>
-  );
-};
+class TopRecipeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topRecipes: [],
+    };
+  }
+
+  componentDidMount() {
+    let context = this;
+    $.ajax({
+      url: '/topRecipes',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('AJAX request to get top recipes succeeded');
+        console.log(data);
+        context.setState({topRecipes: data});
+      },  
+      error: function(err) {
+        console.log('AJAX request to get top recipes failed')
+      }
+    })();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.topRecipes.map((recipe) =>
+          <TopRecipeEntry recipe={recipe}/>
+        )}
+      </div>
+    );
+  }
+}
 
 export default TopRecipeList;
