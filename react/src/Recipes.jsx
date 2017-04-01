bb4f0fdc14a6977918c78c96315ff155f81a209c
Jason An <an.jason.1994@gmail.com>
2017-04-01 16:46:55 -0700

    (refactor) Rerender whole page upon individual recipe click

--- react/src/Recipes.jsx
+++ react/src/Recipes.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import RecipeSearch from './recipeSearch.jsx';
 import VideoSearch from './VideoSearch.jsx';
+import RecipeFromSearch from './RecipeFromSearch.jsx';
 
-const Recipes = (props) => {
-  console.log('looking at the props', props)
-  return (
-    <div className="container">
-      <div className="row" className="video-list-container">
-          {props.videos.length > 0 &&
-            <h2>Recommended Videos</h2>
-          }
-        <ul className="video-list">
-          {props.videos.map((element, index) => <VideoSearch video={element} id={index} key={index} />)}
-        </ul>
+class Recipes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showIndividualRecipe: false,
+      individualRecipe: null,
+    };
+    this.showIndividualRecipe = this.showIndividualRecipe.bind(this);
+  }
+
+  showIndividualRecipe(recipe) {
+    this.setState({
+      showIndividualRecipe: !this.state.showIndividualRecipe,
+      individualRecipe: recipe || null, 
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.showIndividualRecipe &&
+          <div className="container">
+            <div className="row" className="video-list-container">
+                {this.props.videos.length > 0 &&
+                  <h2>Recommended Videos</h2>
+                }
+              <ul className="video-list">
+                {this.props.videos.map((element, index) => <VideoSearch video={element} id={index} key={index} />)}
+              </ul>
+            </div>
+            <div className="results row">
+              <ul>
+                {this.props.recipes.map((element, index) => <RecipeSearch recipe={element.recipe} id={index} key={index} 
+                  showIndividualRecipe={this.showIndividualRecipe}/>)
+                }
+              </ul>
+            </div>
+          </div>    
+        }
+        {this.state.showIndividualRecipe &&
+          <RecipeFromSearch recipe={this.state.individualRecipe} showIndividualRecipe={this.showIndividualRecipe}/>
+        }
       </div>
-      <div className="results row">
-        <ul>
-          {props.recipes.map((element, index) => <RecipeSearch recipe={element.recipe} id={index} key={index}/>)}
-        </ul>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Recipes;
