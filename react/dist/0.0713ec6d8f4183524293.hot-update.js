c33000a80f22770e1e1e3c2b65d8135b731a92e2
billywon <billy.y.won@gmail.com>
2017-03-31 19:06:40 -0700

    (feat) clicking friends' name renders saved recipes and created recipes of that friend

---
+++ react/dist/0.0713ec6d8f4183524293.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate(0,{
+
+/***/ "./react/src/friends.jsx":
+/* unknown exports provided */
+/* all exports used */
+/*!*******************************!*\
+  !*** ./react/src/friends.jsx ***!
+  \*******************************/
+/***/ (function(module, exports) {
+
+"use strict";
+eval("throw new Error(\"Module build failed: SyntaxError: 'return' outside of function (129:4)\\n\\n\\u001b[0m \\u001b[90m 127 | \\u001b[39m      \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mdiv\\u001b[39m\\u001b[33m>\\u001b[39m\\n \\u001b[90m 128 | \\u001b[39m    }\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 129 | \\u001b[39m    \\u001b[36mreturn\\u001b[39m (\\n \\u001b[90m     | \\u001b[39m    \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 130 | \\u001b[39m      template\\n \\u001b[90m 131 | \\u001b[39m    )\\u001b[33m;\\u001b[39m\\n \\u001b[90m 132 | \\u001b[39m  }\\u001b[0m\\n\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9yZWFjdC9zcmMvZnJpZW5kcy5qc3guanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ })
+
+})
