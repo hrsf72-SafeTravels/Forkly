4354443d4327acf905ae43bd1314debc980d96a5
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-02 21:47:14 -0700

    (fix) render top recipes

--- react/src/TopRecipeEntry.jsx
+++ react/src/TopRecipeEntry.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const TopRecipeEntry = (props) => {
   return (
-    <div className="col-lg-3">
-      <h1>{props.recipe.name}</h1>
+    <div className="col-lg-3 top-recipe-entry">
+      {console.log('recipe', props.recipe)}
+      <h1 className="recipe-name">{props.recipe.name}</h1>
       <ul>
         {props.recipe.ingredients.map((ingredient) =>
-          <li>{ingredient}</li>
+          <li>{ingredient.ingredient}</li>
         )}
       </ul>
     </div>
